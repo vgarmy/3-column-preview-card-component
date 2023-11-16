de45a7fac2a79aef634633a6a32131f4c246d0f3
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-16 10:43:48 +0100

    images

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,20 +1,18 @@
-import IconLuxury from '../images/icon-luxury.svg'
-
 const carsinfo = [
     {
       "type": "Sedans",
       "info": "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip",
-      "image": "./images/icon-luxury.svg"
+      "image": "./images/icon-sedans.svg"
     },
     {
       "type": "SUVs",
       "info": "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
-      "image": "./images/icon-sedans.svg"
+      "image": "./images/icon-suvs.svg"
     },
     {
       "type": "Luxury",
       "info": "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
-      "image": {IconLuxury}
+      "image": "./images/icon-luxury.svg"
     },
   ];
 
